c88b2304438fbbb32997503170fe6c5272308cba
MTM2025MT <mohamedtalalemara@gmail.com>
2026-02-08 08:14:59 +0300

    Update changes

--- Api/PrivateAxios.jsx
+++ Api/PrivateAxios.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const Url='https://localhost:7099/api';
+// const Url='https://localhost:7099/api';
+const Url = 'https://potholeapi20251220015420-fkaxfhgze2e6c0eh.germanywestcentral-01.azurewebsites.net/api';
 const PrivateAxios = axios.create({
   baseURL: Url, // Replace with your API base URL
     // timeout: 10000, // Set a timeout limitS
